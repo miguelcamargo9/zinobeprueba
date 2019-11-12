ff2ff4adbe97df742c0be65fdd1f0bf7cda2b20a
Miguel Camargo <miguelcamargo9@gmail.com>
2019-11-12 12:33:01 -0500

    Config the Front Controller

---
+++ public/index.php
@@ -0,0 +1,51 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Aura\Router\RouterContainer;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'zinobeprueba',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_FILES
+);
+
+$routerContainer = new RouterContainer();
+$map = $routerContainer->getMap();
+$map->get('index', '/zinobeprueba/', '../index.php');
+$map->get('addUser', '/zinobeprueba/addUser', '../addUser.php');
+
+$matcher = $routerContainer->getMatcher();
+$route = $matcher->match($request);
+
+if (!$route) {
+    echo 'PAGO 404';
+} else {
+    require $route->handler;
+}
+
+
+
+// require '../index.php';
